81f499fdcde6753eb7b47c8deee7964d2f3d823c
Muh Zadi <zadiansyah@gmail.com>
2017-07-08 15:32:04 +0700

    Add files via upload

---
+++ DataFish.java
@@ -0,0 +1,12 @@
+package com.zadi.pencarian2;
+
+/**
+ * Created by Muh. Zadi on 7/8/2017.
+ */
+
+public class DataFish {
+    public String fishName;
+    public String catName;
+    public String sizeName;
+    public int price;
+}
